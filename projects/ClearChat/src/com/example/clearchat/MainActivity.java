f8b2970d61dbdcd3f605077c0a11ef3df3b9d0a6
Michael Sprague <msprague@clearblade.com>
2014-02-05 13:52:51 -0600

    updated app to relect latest tutorial version

--- projects/ClearChat/src/com/example/clearchat/MainActivity.java
+++ projects/ClearChat/src/com/example/clearchat/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import com.clearblade.platform.api.ClearBlade;
 import com.clearblade.platform.api.ClearBladeException;
 import com.clearblade.platform.api.DataCallback;
+import com.clearblade.platform.api.InitCallback;
 import com.clearblade.platform.api.Item;
 import com.clearblade.platform.api.Query;
 
@@ -28,26 +29,34 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	public final static String APPKEY = "eac9d0aa0ae0dcd7b1e496f4ddde01";
-    public final static String APPSECRET = "EAC9D0AA0AAAA886B5B4BBAFC6E701";
+	public final static String SYSTEMKEY = "eac9d0aa0ae0dcd7b1e496f4ddde01";
+    public final static String SYSTEMSECRET = "EAC9D0AA0AAAA886B5B4BBAFC6E701";
     
     public final static String USERCOLLECTIONID = "90cad0aa0ac8d8bf89ff8afea432";
     
-//    public final static String APPKEY = "5277bd628ab3a37ce7f6f061";
-//    public final static String APPSECRET = "0D2N19VB3FPYJYEBSOI4LVG6M97PKX";
-//         
-//    public final static String USERCOLLECTIONID = "5277bd878ab3a37ce7f6f062";
-    
-    
-	public void login(View view){
+	public void initCBAPI(View view){
+		
+		
+		InitCallback initCallback = new InitCallback(){
+	        @Override
+	        public void done(boolean results){
+	            //initialization successful
+	            login();
+	        }
+	        @Override
+	        public void error(ClearBladeException exception){ 
+	            //initialization failed, given a ClearBladeException with the cause
+	        }
+	    };
+
+	    //Initialize the platform
+	    ClearBlade.initialize(SYSTEMKEY, SYSTEMSECRET, initCallback);
+		
+
+	}
+	
+	public void login(){
 		final EditText username_text = (EditText) findViewById(R.id.username);
-		
-		//Initialize the platform
-		ClearBlade.initialize(APPKEY, APPSECRET);
-		
-//		ClearBlade.setUri("https://dev.clearblade.com");
-//		ClearBlade.setMessageUrl("tcp://messaging.clearblade.com:1883");
-		//Create a Query object to search for the user in the collection
 		Query query = new Query();
 		query.setCollectionId(USERCOLLECTIONID);
 		query.equalTo("username", username_text.getText().toString());
